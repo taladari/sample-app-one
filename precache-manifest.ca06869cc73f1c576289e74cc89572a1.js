da3d637203ddce484fd1f3e45bcdccec1ed54fbd
Tal Adari <tal0392@gmail.com>
2019-06-20 00:23:42 +0300

    Updates

---
+++ precache-manifest.ca06869cc73f1c576289e74cc89572a1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa947a9a672e34f78970c0319ff6f1a8",
+    "url": "/sample-app-one/index.html"
+  },
+  {
+    "revision": "37a186be52d5c93f94d6",
+    "url": "/sample-app-one/static/css/main.273dac97.chunk.css"
+  },
+  {
+    "revision": "96454847e5560087d32a",
+    "url": "/sample-app-one/static/js/2.f3ef9e25.chunk.js"
+  },
+  {
+    "revision": "37a186be52d5c93f94d6",
+    "url": "/sample-app-one/static/js/main.1094111d.chunk.js"
+  },
+  {
+    "revision": "bd84029bb7e4b2e0dda1",
+    "url": "/sample-app-one/static/js/runtime~main.ef3a01ac.js"
+  },
+  {
+    "revision": "f15edcd4a711c64464ee33e698cc13b3",
+    "url": "/sample-app-one/static/media/Phone.f15edcd4.png"
+  },
+  {
+    "revision": "46a41422d15bde6df20db9929f6e6a72",
+    "url": "/sample-app-one/static/media/backg.46a41422.png"
+  },
+  {
+    "revision": "dacb36f64c425982c253448951d79eaf",
+    "url": "/sample-app-one/static/media/check.dacb36f6.svg"
+  },
+  {
+    "revision": "9993a4308dd443b7f7b01ee996aea153",
+    "url": "/sample-app-one/static/media/close.9993a430.svg"
+  },
+  {
+    "revision": "cb48b1935cde881e5af9a73dd4ecd92b",
+    "url": "/sample-app-one/static/media/data.cb48b193.svg"
+  },
+  {
+    "revision": "5422a688b98bf80b02cf69fa283bbf4a",
+    "url": "/sample-app-one/static/media/download.5422a688.svg"
+  },
+  {
+    "revision": "8c856a6a3ab70f90d3f1c2f92c2342fa",
+    "url": "/sample-app-one/static/media/facebook.8c856a6a.svg"
+  },
+  {
+    "revision": "9988dd022701e820cc7bc35859bb2d8a",
+    "url": "/sample-app-one/static/media/featuresbackg.9988dd02.png"
+  },
+  {
+    "revision": "031cf91d704defc2bdb8d656be52e995",
+    "url": "/sample-app-one/static/media/firstImg.031cf91d.png"
+  },
+  {
+    "revision": "6f7f616f44e4c4244580338661f876d9",
+    "url": "/sample-app-one/static/media/instagram.6f7f616f.svg"
+  },
+  {
+    "revision": "48a2133dd5c34ad46cc6ab6ff06c03ec",
+    "url": "/sample-app-one/static/media/linkedin.48a2133d.svg"
+  },
+  {
+    "revision": "fc2578b5b60211bb7eccebd4f07ca2fa",
+    "url": "/sample-app-one/static/media/phone2.fc2578b5.png"
+  },
+  {
+    "revision": "e475b8399e30762612f71fc03a83b787",
+    "url": "/sample-app-one/static/media/secondImg.e475b839.png"
+  },
+  {
+    "revision": "d22abc47aca2cdbf9b334a81467e81bf",
+    "url": "/sample-app-one/static/media/support.d22abc47.svg"
+  },
+  {
+    "revision": "0729c42ac6c88ea12311c6989df785d6",
+    "url": "/sample-app-one/static/media/twitter.0729c42a.svg"
+  }
+]);
